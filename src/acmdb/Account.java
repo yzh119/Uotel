1576336aa84d7e58e95765d943cfdcb8f6ef3a9f
Zihao Ye <yzhmoonlight@gmail.com>
2017-06-06 21:00:47 +0800

    update suggestion

--- src/acmdb/Account.java
+++ src/acmdb/Account.java
@@ -71,17 +71,17 @@ public class Account {
 
     public static List<List<String>> getRecommendations(String username) throws Exception {
         return Utility.query(
-            "SELECT distinct new_th.* FROM " +
-            "user u1, user u2, " +
-            "visit v1, visit v2, visit all2, " +
-            "reservation r1, reservation r2, reservation allr2, " +
-            "TH common_th, (SELECT * FROM TH th natural join (SELECT uid, COUNT(DISTINCT user_name) AS like_cnt FROM favorite GROUP BY uid) as th_info) AS new_th " +
-            "WHERE " +
-            "u1.login_name = '" + username + "' and u1.login_name <> u2.login_name and " +
-            "v1.user_name = u1.login_name and v1.rid = r1.rid and v1.user_name = r1.user_name and r1.uid = common_th.uid and " +
-            "v2.user_name = u2.login_name and v2.rid = r2.rid and v2.user_name = r2.user_name and r2.uid = common_th.uid and " +
-            "all2.user_name = u2.login_name and all2.rid = allr2.rid and all2.user_name = allr2.user_name and new_th.uid = allr2.uid " +
-            "ORDER by new_th.like_cnt DESC"
+            "SELECT new_th.*, count(*) as like_cnt FROM " +
+                "user u1, user u2, " +
+                "visit v1, visit v2, visit all2, " +
+                "reservation r1, reservation r2, reservation allr2, " +
+                "TH common_th, TH new_th " +
+                "WHERE " +
+                "u1.login_name = '" + username + "' and u1.login_name <> u2.login_name and " +
+                "v1.user_name = u1.login_name and v1.rid = r1.rid and v1.user_name = r1.user_name and r1.uid = common_th.uid and " +
+                "v2.user_name = u2.login_name and v2.rid = r2.rid and v2.user_name = r2.user_name and r2.uid = common_th.uid and " +
+                "all2.user_name = u2.login_name and all2.rid = allr2.rid and all2.user_name = allr2.user_name and new_th.uid = allr2.uid " +
+                "group by new_th.uid "
         );
     }
 
