09edd04a20248b98deaac777aefcc5855e24a68e
Zhijian Liu <zhijianliu.cs@gmail.com>
2017-06-06 21:28:18 +0800

    -

--- src/acmdb/Available.java
+++ src/acmdb/Available.java
@@ -4,12 +4,10 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Available {
-    public static void remove(int uid, String startTime, String endTime) throws Exception {
+    public static void remove(int id, String start, String end) throws Exception {
         Connector connector = new Connector();
-        Statement stmt = connector.statement;
-        String statement = "DELETE a FROM available a WHERE a.uid = " + uid + " and " +
-                "a.start_date='" + startTime.substring(0, 10) + "' and a.end_date='" + endTime.substring(0, 10) + "'";
-        stmt.execute(statement);
+        Statement statement = connector.statement;
+        statement.execute("DELETE a FROM available a WHERE a.uid = " + id + " AND a.start_date='" + start.substring(0, 10) + "' AND a.end_date='" + end.substring(0, 10) + "'");
         connector.close();
     }
 
@@ -17,27 +15,18 @@ public class Available {
         Connector connector = new Connector();
         Statement statement = connector.statement;
 
-        ResultSet rs;
-        rs = statement.executeQuery("SELECT * FROM period p WHERE p.start_date='" + start + "' and " + "p.end_date='" + end + "'");
-
-        if (!rs.next()) {
-            try {
-                statement.execute("INSERT INTO period values('" + start + "','" + end + "')");
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw e;
-            }
+        ResultSet result;
+        result = statement.executeQuery("SELECT * FROM period p WHERE p.start_date='" + start + "' and " + "p.end_date='" + end + "'");
+        if (!result.next()) {
+            statement.execute("INSERT INTO period values('" + start + "','" + end + "')");
         }
-
-        rs = statement.executeQuery("SELECT * FROM available a " +
+        result = statement.executeQuery("SELECT * FROM available a " +
             "WHERE  a.uid = " + uid +
-            " and    ((a.start_date <= '" + start + "' and a.end_date >= '" + start + "')" +
-            " or     (a.start_date <= '" + end + "' and a.end_date >= '" + end + "'))");
-
-        if (rs.next()) {
+            " AND    ((a.start_date <= '" + start + "' and a.end_date >= '" + start + "')" +
+            " OR     (a.start_date <= '" + end + "' and a.end_date >= '" + end + "'))");
+        if (result.next()) {
             throw new Exception("Overlay period!");
         }
-
         statement.execute("INSERT INTO available values(" + uid + ",'" + start + "','" + end + "')");
         connector.close();
     }
