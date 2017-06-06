b76b906582970d8e3dbacf7273575660503c96e7
Zihao Ye <yzhmoonlight@gmail.com>
2017-06-06 22:03:41 +0800

    upate

--- src/acmdb/Account.java
+++ src/acmdb/Account.java
@@ -65,17 +65,21 @@ public class Account {
 
     public static List<List<String>> getRecommendations(String username) throws Exception {
         return Utility.query(
-                "SELECT * from (SELECT new_th.*, count(*) as like_cnt FROM " +
+            "SELECT * FROM ( " +
+                "SELECT new_th.*, count(*) as like_cnt FROM " +
                 "user u1, user u2, " +
-                "visit v1, visit v2, visit all2, " +
-                "reservation r1, reservation r2, reservation allr2, " +
-                "TH common_th, TH new_th " +
+                "visit all2, " +
+                "reservation allr2, " +
+                "TH new_th " +
                 "WHERE " +
                 "u1.login_name = '" + username + "' and u1.login_name <> u2.login_name and " +
-                "v1.user_name = u1.login_name and v1.rid = r1.rid and v1.user_name = r1.user_name and r1.uid = common_th.uid and " +
-                "v2.user_name = u2.login_name and v2.rid = r2.rid and v2.user_name = r2.user_name and r2.uid = common_th.uid and " +
+                "exists (select * from visit v1, reservation r1, visit v2, reservation r2 where " +
+                "v1.user_name = u1.login_name and v1.rid = r1.rid and v1.user_name = r1.user_name and " +
+                "v2.user_name = u2.login_name and v2.rid = r2.rid and v2.user_name = r2.user_name and " +
+                "r1.uid = r2.uid " +
+                ") and " +
                 "all2.user_name = u2.login_name and all2.rid = allr2.rid and all2.user_name = allr2.user_name and new_th.uid = allr2.uid " +
-                "group by new_th.uid) as result order by result.like_cnt DESC"
+                "group by new_th.uid) as result order by result.like_cnt DESC "
         );
     }
 
